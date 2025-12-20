b90677dffee1e3da1a6165ed52a9d302ce1cf736
cat658011 <cat658011@gmail.com>
2025-12-21 01:35:28 +0500

    Update Flowseal/zapret-discord-youtube to 6b33dd5600d5ed699997f1c455798146474e6d53

--- main_script.sh
+++ main_script.sh
@@ -74,7 +74,7 @@ setup_repository() {
             log "Обновление репозитория..."
             rm -rf "$REPO_DIR"
             git clone "$REPO_URL" "$REPO_DIR" || handle_error "Ошибка при клонировании репозитория"
-            cd "$REPO_DIR" && git checkout a609396772dfe2a3c85b0cec8c314ff9ac96a5c0 && cd ..
+            cd "$REPO_DIR" && git checkout 6b33dd5600d5ed699997f1c455798146474e6d53 && cd ..
             # rename_bat.sh
             chmod +x "$BASE_DIR/rename_bat.sh"
             rm -rf "$REPO_DIR/.git"
@@ -85,7 +85,7 @@ setup_repository() {
     else
         log "Клонирование репозитория..."
         git clone "$REPO_URL" "$REPO_DIR" || handle_error "Ошибка при клонировании репозитория"
-        cd "$REPO_DIR" && git checkout a609396772dfe2a3c85b0cec8c314ff9ac96a5c0 && cd ..
+        cd "$REPO_DIR" && git checkout 6b33dd5600d5ed699997f1c455798146474e6d53 && cd ..
         # rename_bat.sh
         chmod +x "$BASE_DIR/rename_bat.sh"
         rm -rf "$REPO_DIR/.git"
