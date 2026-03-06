e410317f48ab301c5d223626681b9a446ceecbb9
geomi737 <lesh.yt.cool@gmail.com>
2026-03-06 17:51:31 +0300

    Ipset switch feature added
    CI failure test
    
    mode_ipset use in rename_ipset function
    
    --switch-ipset flag remove

---
+++ src/lib/ipswitch.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+rename_ipset(){
+    local ipset="$REPO_DIR/lists/ipset-all.txt"
+    local bipset="$REPO_DIR/lists/ipset-all.txt.backup"
+    if [[ $# != 1 ]]; then
+        handle_error "Не приложен mode или приложено более двух аргументов"
+    fi
+
+    mode="$1"
+
+    if [[ "$mode" == "None (Только Lists)" ]]; then
+        rm -rf "$ipset"
+        touch "$ipset"
+        echo "Выбранный режим - $(mode_ipset)"
+    elif [[ "$mode" == "Any (Весь траффик)" ]]; then
+        if [ -f "$bipset" ]; then
+            rm -rf "$ipset"
+            cp "$bipset" "$ipset"
+            echo "Выбранный режим - $(mode_ipset)"
+            return 0
+        fi
+        handle_error "Не найден бекап, переустановите zapret стратегии"
+    else
+        if [ -f "$bipset" ]; then
+            rm -rf "$bipset"
+        fi
+        cp "$ipset" "$bipset"
+        echo "203.0.113.113/32" > "$ipset"
+        echo "Выбранный режим - $(mode_ipset)"
+        return 0
+    fi
+}
+
+mode_ipset(){
+    local ipset="$REPO_DIR/lists/ipset-all.txt"
+
+    if [[ $(wc -l < "$ipset") == 1 ]] && [[ $(cat "$ipset") == "203.0.113.113/32" ]]; then
+        echo "None (Только Lists)"
+    elif [[ $(wc -l < "$ipset") == 0 ]]; then
+        echo "Any (Весь траффик)"
+    else
+        echo "Loaded (Только то что в Ipset)"
+    fi
+}
